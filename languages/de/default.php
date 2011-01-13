99f3e30a2dc8057e117ed753b628569c4175ab9e
taca <taca>
2011-01-13 07:47:56 +0000

    update.

---
+++ languages/de/default.php
@@ -0,0 +1,8 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+$GLOBALS['TL_LANG']['FFL']['tText'] = array('Small extended text field', 'A text field with small extension.');
+
+$GLOBALS['TL_LANG']['ERR']['email-noidn'] = 'Bitte geben Sie eine g«ältige E-Mail-Adresse nicht ohne Internationalisierung Domainnamen!';
+$GLOBALS['TL_LANG']['ERR']['url-noidn'] =   'Bitte geben Sie eine g«ältige URL ohne Internationalisierung Domainnamen!';
+
+?>
--- languages/ja/tl_form_field.php
+++ languages/ja/tl_form_field.php
@@ -1,6 +1,6 @@
 <?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
 
-/* 国際化ドメインを使用しない入力検査 */
+$GLOBALS['TL_LANG']['tl_form_field']['tText'] = array('小拡張テキスト入力', 'ちょっとした拡張を加えたテキスト入力の欄です。');
 
 $GLOBALS['TL_LANG']['tl_form_field']['email-noidn'] = 
 	array('非国際化電子メールアドレス', 
