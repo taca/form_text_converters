a79429a5ffa249fe1c8e1634aeae633b88c0d35d
taca <taca>
2011-01-14 09:35:26 +0000

    * Add a record to table for currently enabled charracter conversion. * Change hDakuten by hKatakana's value.

--- dca/tl_form_field.php
+++ dca/tl_form_field.php
@@ -3,11 +3,22 @@
 /**
  * Palettes
  */
-$GLOBALS['TL_DCA']['tl_form_field']['palettes']['lText'] = '{type_legend},type,name,label;{fconfig_legend},mandatory,rgxp,maxlength;{ltext_legend:hide},normalize,alpha,digit,alnum,space,hKatakana,hDakuten,fKatakana,fHiragana;{expert_legend:hide},value,class,accesskey;{submit_legend},addSubmit';
+$GLOBALS['TL_DCA']['tl_form_field']['palettes']['lText'] = '{type_legend},type,name,label;{fconfig_legend},mandatory,rgxp,maxlength;{ltext_legend:hide},conversion;{expert_legend:hide},value,class,accesskey;{submit_legend},addSubmit';
+
+$GLOBALS['TL_DCA']['tl_form_field']['palettes']['__selector__'][] = 'conversion';
+$GLOBALS['TL_DCA']['tl_form_field']['subpalettes']['conversion'] = 'normalize,alpha,digit,alnum,space,hKatakana,hDakuten,fKatakana,fHiragana';
 
 /**
  * Fields
  */
+$GLOBALS['TL_DCA']['tl_form_field']['fields']['conversion'] = array
+(
+    'label'     => &$GLOBALS['TL_LANG']['tl_form_field']['conversion'],
+	'inputType' => 'checkbox',
+    'reference' => &$GLOBALS['TL_LANG']['tl_form_field'],
+    'eval'      => array('submitOnChange'=>true, 'tl_class' => 'w50 m12')
+);
+
 $GLOBALS['TL_DCA']['tl_form_field']['fields']['normalize'] = array
 (
     'label'     => &$GLOBALS['TL_LANG']['tl_form_field']['normalize'],
@@ -66,7 +77,11 @@ $GLOBALS['TL_DCA']['tl_form_field']['fields']['hDakuten'] = array
 (
     'label'     => &$GLOBALS['TL_LANG']['tl_form_field']['hDakuten'],
 	'inputType' => 'checkbox',
-    'eval'      => array('tl_class' => 'w50 m12')
+    'eval'      => array('tl_class' => 'w50 m12'),
+    'save_callback' => array
+    (
+        array('tl_lform_field', 'validateForm')
+    ),
 );
 
 $GLOBALS['TL_DCA']['tl_form_field']['fields']['fKatakana'] = array
@@ -90,5 +105,19 @@ $GLOBALS['TL_DCA']['tl_form_field']['fields']['fHiragana'] = array
 
 $GLOBALS['TL_DCA']['tl_form_field']['fields']['rgxp']['options'][] = 'email-noidn';
 $GLOBALS['TL_DCA']['tl_form_field']['fields']['rgxp']['options'][] = 'url-noidn';
+
+class tl_lform_field extends tl_form_field
+{
+    public function validateForm($varInput, $dc)
+    {
+        if (!empty($varInput)) {
+            $l = $dc->Input->post('hKatakana');
+            if (empty($l) || $l == 'none') {
+                $varInput = '';
+            }
+        }
+        return $varInput;
+    }
+}
 
 ?>
