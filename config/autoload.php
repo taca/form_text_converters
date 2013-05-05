5c716bc80755f970c0be59d9f5dde9a03448c1e9
Takahiro Kambe <taca@back-street.net>
2013-05-05 23:26:45 +0900

    Add autoload.ini and autoload.php.

---
+++ config/autoload.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2013 Leo Feyer
+ * 
+ * @package Form_text_converters
+ * @link    https://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	'FormTextAreaConvert'  => 'system/modules/form_text_converters/FormTextAreaConvert.php',
+	'FormTextFieldConvert' => 'system/modules/form_text_converters/FormTextFieldConvert.php',
+	'MiscTextConverters'   => 'system/modules/form_text_converters/MiscTextConverters.php',
+));
